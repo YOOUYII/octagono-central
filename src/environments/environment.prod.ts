89f1d36b0316bf6c1b47657ca1c5ea96db0b32df
Rodrigo Vázquez Rico <rodririco213@gmail.com>
2026-03-26 16:10:34 -0600

    chore: update environment.prod.ts with final gateway URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-gateway-en-render.onrender.com/api' // Cambia esto por tu URL real de Render
+  apiUrl: 'https://octagono-gateway.onrender.com'
 };

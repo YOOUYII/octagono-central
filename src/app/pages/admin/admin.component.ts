655d51e0bfea1ebc4227e33d2d7231ed182032ee
Rodrigo Vázquez Rico <rodririco213@gmail.com>
2026-04-16 15:35:54 -0600

    Forzar la recarga

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, ApplicationRef, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from '../../layouts/header/header.component';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
@@ -9,4 +10,27 @@ import { HeaderComponent } from '../../layouts/header/header.component';
   imports: [CommonModule, RouterModule, HeaderComponent],
   templateUrl: './admin.component.html'
 })
-export class AdminComponent {}
+export class AdminComponent implements OnDestroy {
+  private router = inject(Router);
+  private appRef = inject(ApplicationRef);
+  private navSubscription: Subscription;
+
+  constructor() {
+    // Escuchar eventos de navegación para forzar un refresco de la UI
+    // Esto soluciona problemas donde el router-outlet no se actualiza en el primer clic
+    this.navSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Forzar un ciclo de detección de cambios en toda la aplicación
+      setTimeout(() => {
+        this.appRef.tick();
+      }, 0);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.navSubscription) {
+      this.navSubscription.unsubscribe();
+    }
+  }
+}
